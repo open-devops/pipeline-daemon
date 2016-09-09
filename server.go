29f613f60361d9c05647fc591374ecf154220447
open-devops <open.devops@gmail.com>
2016-09-09 14:57:06 +0800

    Add docker compose support

--- server.go
+++ server.go
@@ -12,12 +12,13 @@ const (
 )
 
 func main() {
-	log.Printf("Pipeline Daemon Server started!")
+	log.Println("Pipeline Daemon Server started!")
 
 	router := sw.NewRouter()
 
 	daemonBindingPort := os.Getenv("PIPELINE_DAEMON_PORT")
 	if len(daemonBindingPort) == 0 {
+		log.Println("Used default daemon binding port!")
 		daemonBindingPort = DEFAULT_DAEMON_PORT
 	}
 
