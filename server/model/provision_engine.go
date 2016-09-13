a9c111eb6b160ee53765a50b1149aabd074a0b78
open-devops <open.devops@gmail.com>
2016-09-13 09:20:06 +0800

    Add get provision support

--- server/model/provision_engine.go
+++ server/model/provision_engine.go
@@ -67,7 +67,7 @@ func DeleteProvision(pipelineInfo *types.PipelineInfo) error {
 		return err
 	}
 	if existed {
-		if _, err := HandlePipelineAction(pipelineInfo, ACTION_STOP, nil); err != nil {
+		if _, err := HandlePipelineAction(pipelineInfo, ACTION_STOP, ""); err != nil {
 			return err
 		}
 		if err := os.RemoveAll(engineParentPath); err != nil {
